1191e5cb63d7b5bb2952206f9cc728660e4edb5c
study0918 <beyondthestars@yeah.net>
2018-09-18 23:01:48 +0800

    简洁的控制器

--- js/05.js
+++ js/05.js
@@ -23,3 +23,32 @@ app.controller("ChildController", ["$scope", function ($scope) {
         $scope.person.name = "Ari Lerner";
     }
 }])
+//复杂的控制器
+app.controller("complexController",["$scope",function($scope){
+    $scope.shouldShowLogin=true;
+    $scope.showLogin=function(){
+        $scope.shouldShowLogin=!$scope.shouldShowLogin;
+    };
+    $scope.clickButton=function(){
+        $('#btn span').html("Clicked");
+    };
+    $scope.onLogin=function(user){
+        $http({
+            method:'POST',
+            url:'/login',
+            data:{
+                user:user
+            }
+        }).success(function(data){
+            //user
+        })
+    }
+}])
+
+//简洁的控制器
+app.controller("simpleController",["$scope",function(){
+    //内容可以被指令控制
+    $scope.onLogin=function(user){
+        UserSrv.runLogin(user);
+    }
+}])
