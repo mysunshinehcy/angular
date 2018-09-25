ae8f58502ba756ad387d2a301787a265dd7168e8
study0918 <beyondthestars@yeah.net>
2018-09-25 20:02:53 +0800

    Create 11.js

---
+++ js/11.js
@@ -0,0 +1,6 @@
+angular.module('myApp', []).run(function ($rootScope, $timeout) {
+    $rootScope.isDisabled = true;
+    $timeout(function () {
+        $rootScope.isDisabled = false;
+    }, 5000);
+})
