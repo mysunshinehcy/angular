07014500c7a7a64047ad33f4d57d5afbfafe3d78
study0918 <beyondthestars@yeah.net>
2018-09-25 20:03:16 +0800

    Create chapter10.js

---
+++ js/chapter10.js
@@ -0,0 +1,13 @@
+/**
+ * 布尔属性
+ * 根据HTML标准的定义，布尔属性代表一个true或false值。当这个属性出现时，这个属性的值就是true(无论实际定义的值是什么)。如果未出现
+ * 这个属性的值就是false。
+ */
+
+/**
+ * ng-disabled:使用ng-disabled可以把disabled属性绑定到以下表单输入字段上
+ * <input>(text、checkbox、radio、number、url、email、submit)
+ * <textarea>
+ * <select>
+ * <button>
+ */
