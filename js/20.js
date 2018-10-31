c20776f6006e627627a8ec2bb5cdbc33b7b00a45
study0918 <beyondthestars@yeah.net>
2018-11-01 00:02:42 +0800

    从仓库获取事件

--- js/20.js
+++ js/20.js
@@ -1,20 +1,27 @@
-angular.module('myApp',[]).controller('DashboardController',['$scope','githubService',function($scope,UserService){
+angular.module('myApp', []).controller('DashboardController', ['$scope', 'githubService', function ($scope, UserService) {
     // GithubService的getPullRequests()方法
     // 返回了一个promise
-    User.getPullRequest(123).then(function(data){
-        $scope.pullRequests=data.data;
+    User.getPullRequest(123).then(function (data) {
+        $scope.pullRequests = data.data;
     })
-}]).factory('GithubService',function($q,$http){
+}]).factory('GithubService', function ($q, $http) {
     //从仓库获取事件
-    var getEventsFromRepo=function(){
+    var getEventsFromRepo = function () {
         //任务
     }
-    var service={
-        makeMultipRequests:function(repos){
-            var d=$q.defer(),percentComplete=0,output=[];
-            for(var i=0;i<repos.length;i++){
-                
+    var service = {
+        makeMultipRequests: function (repos) {
+            var d = $q.defer(),
+                percentComplete = 0,
+                output = [];
+            for (var i = 0; i < repos.length; i++) {
+                output.push(getEventsFromRepo(repos[i]));
+                percentComplete = (i + 1) / repos.length * 100;
+                d.notify(percentComplete);
             }
+            d.resolve(output);
+            return d.promise;
         }
     }
+    return service;
 })
