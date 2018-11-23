ed9dbd5c2072c5990408516537a593e27165fbb6
study0918 <beyondthestars@yeah.net>
2018-11-23 23:26:19 +0800

    'weather'

--- chrome app/js/app.js
+++ chrome app/js/app.js
@@ -1,6 +1,7 @@
-angular.module('myApp', [])
-    .controller('MainController', function ($scope, $timeout) {
+angular.module('myApp', ['ngRoute'])
+    .controller('MainController', function ($scope, $timeout, Weather) {
         //构建date对象
+        //https://blog.csdn.net/zangxueyuan88/article/details/81100054
         $scope.date = {};
         //更新函数
         var updateTime = function () {
@@ -9,18 +10,49 @@ angular.module('myApp', [])
         }
         //启动更新函数
         updateTime();
+        $scope.weather = {};
+        console.log($scope.weather);
+        Weather.getWeatherForecast("autoip").then(function (data) {
+            $scope.weather.forcast = data;
+        })
     })
     .provider('Weather', function () {
-        console.log(this);
         var apiKey = "";
         this.setApiKey = function (key) {
             if (key) {
                 this.apiKey = key;
             }
         };
-        this.$get = function ($http) {
+        this.$get = function ($q, $http) {
+            var self = this;
             return {
                 //服务对象
+                //创建API来调用Weather服务，帮我们从Wunderground的API中获取最新的预测数据
+                //我们将会创建自己的，可以用来在视图中解析数据的promise，因为我们只希望从API调用中返回相关的结果
+                getWeatherForecast: function (city) {
+                    var d = $q.defer();
+                    $http({
+                        method: 'GET',
+                        url: self.getUrl("forecast", city),
+                        cache: true
+                    }).success(function (data) {
+                        //Wunderground API返回
+                        //嵌套在forecast.simpleforcast属性内的forecasts对象
+                        d.resolve(data.forecast.simpleforecast);
+                    }).error(function (err) {
+                        d.reject(err);
+                    })
+                    //采坑，可以在deferred对象上以属性的方式访问promise，deferred.promise，而不是deferred.promise()
+                    return d.promise;
+                }
             }
+        };
+        this.getUrl = function (type, ext) {
+            return "http://api.wunderground.com/api/" + this.apiKey + "/" + type + "/q/" + ext + ".json";
         }
+    })
+    .config(function (WeatherProvider) {
+        //此处YOUR_API_KEY赋值为7528801ea9a07daa
+        //WeatherProvider.setApiKey('YOUR_API_KEY');
+        WeatherProvider.setApiKey('7528801ea9a07daa');
     })
